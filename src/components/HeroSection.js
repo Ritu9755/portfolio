b97b156d791fb239601b963d43320f7b68d37a57
Ritu <ritusawant363@gmail.com>
2025-02-10 17:39:31 +0530

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -105,7 +105,7 @@ const HeroSection = ({ nav, handleNav }) => {
 			initial='hidden'
 			whileInView='visible'>
 			<a 
-				href="https://drive.google.com/file/d/1X-Dv2X5cx-8PBUxuHn6krRE3BGJgsXdP/view?usp=drive_link" 
+				href="https://drive.google.com/file/d/1Q0A2a4xB-4jp-vHCxcV7S7C9KdUcVcQC/view?usp=drive_link" 
 				target="_blank" 
 				className='hero-contact' 
 				rel="noopener noreferrer">
