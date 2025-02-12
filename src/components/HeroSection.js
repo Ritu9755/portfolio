350cfe8977509ef752b64b96b613fa0e67d0211f
Ritu <ritusawant363@gmail.com>
2025-02-12 15:12:37 +0530

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -101,17 +101,20 @@ const HeroSection = ({ nav, handleNav }) => {
 				</p>
 			</motion.div>
 			<motion.span
-			variants={contactVariants}
-			initial='hidden'
-			whileInView='visible'>
-			<a 
-				href=https:"https://drive.google.com/file/d/1UOK_eZqU5SkyMc1z0_YrfG1CtSYC_PhH/view?usp=drive_link" 
-				target="_blank" 
-				className='hero-contact' 
-				rel="noopener noreferrer">
-				Download CV <BiDownload className='cv-icon' />
-			</a>
-			</motion.span>
+				  variants={contactVariants}
+				  initial="hidden"
+				  whileInView="visible"
+				>
+				  <a 
+				    href="https://drive.google.com/file/d/1UOK_eZqU5SkyMc1z0_YrfG1CtSYC_PhH/view?usp=drive_link" 
+				    target="_blank" 
+				    className="hero-contact" 
+				    rel="noopener noreferrer"
+				  >
+				    Download CV <BiDownload className="cv-icon" />
+				  </a>
+				</motion.span>
+
 
 		</div>
 	);
